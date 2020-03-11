d55a257b2bdf26857f53134beaf89c7c9905c207
dtand <dtanderson005@gmail.com>
2020-03-10 22:29:41 -0400

    new

---
+++ n64-bet-app/build/precache-manifest.f7bd0919ff4471eba7fe81191bb11d41.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f6cba62eba5fec18312606b99fb7f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3430f0dc779fc360881d",
+    "url": "/static/css/2.d61d0060.chunk.css"
+  },
+  {
+    "revision": "61fac4f35ff7b8ca934f",
+    "url": "/static/css/main.ea72f345.chunk.css"
+  },
+  {
+    "revision": "3430f0dc779fc360881d",
+    "url": "/static/js/2.82488775.chunk.js"
+  },
+  {
+    "revision": "39e0ceac42ed006f9d6cc7d7a6458047",
+    "url": "/static/js/2.82488775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fac4f35ff7b8ca934f",
+    "url": "/static/js/main.c6848abe.chunk.js"
+  },
+  {
+    "revision": "2586a188e77c601e808e",
+    "url": "/static/js/runtime-main.322eff87.js"
+  },
+  {
+    "revision": "7e03e44e1f90aca99b000a6af7a45412",
+    "url": "/static/media/captain-falcon.7e03e44e.png"
+  },
+  {
+    "revision": "4317ea842f738ec732fe2f1c6f215df1",
+    "url": "/static/media/donkey-kong.4317ea84.png"
+  },
+  {
+    "revision": "2b342be53dc8bc0e8883150728fd484d",
+    "url": "/static/media/fox.2b342be5.png"
+  },
+  {
+    "revision": "a8bee10517dc55bcfe80068206929f11",
+    "url": "/static/media/jigglypuff.a8bee105.png"
+  },
+  {
+    "revision": "98999074a3a1e41a4ba8ebab27fd7f98",
+    "url": "/static/media/kirby.98999074.png"
+  },
+  {
+    "revision": "967d372d5a58c6f68ead559625a178a4",
+    "url": "/static/media/link.967d372d.png"
+  },
+  {
+    "revision": "37dc50ca6682fa668b09923db598a4b9",
+    "url": "/static/media/luigi.37dc50ca.png"
+  },
+  {
+    "revision": "914bb2b4dd3221c92fc5d6d2573a58ce",
+    "url": "/static/media/mario.914bb2b4.png"
+  },
+  {
+    "revision": "a2a6917e39928f149e16def6132bc394",
+    "url": "/static/media/ness.a2a6917e.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "7918bdf2ae15cee54d246f84a5f39627",
+    "url": "/static/media/pikachu.7918bdf2.png"
+  },
+  {
+    "revision": "bb8c010d4a674a9b8e869a4570aaff3e",
+    "url": "/static/media/samus.bb8c010d.png"
+  },
+  {
+    "revision": "6e84f65e7239b57d544bb0c6fbc53ca0",
+    "url": "/static/media/yoshi.6e84f65e.png"
+  }
+]);
